f4ec25d7fdbc522b136f6b2ae1e130367d5094b0
Daniela Feitosa <danielafeitosa@gmail.com>
2021-11-07 14:34:07 -0800

    Generalize percussion set architecture (#9)
    * Get soundfont from config file
    * Define default values as global const
    * Hardcode the constants:
    DEFAULT_SOUNDFONT: defined as "Doumbek-Faisal"
    DEFAULT_INSTRUMENT: defined as "doumbek'
    DEFAULT_BUTTONS: set the buttons with the 6 first codes defined on soundfonts file
    * Organize the soundfonts file as suggested:
    soundfonts => instruments => mapping of MIDI keys to percussion names
    * Initialize app on `DOMContentLoaded` event
    
    Resolves #6

--- CREDITS.md
+++ CREDITS.md
@@ -1,1 +1,2 @@
 Drum by supalerk laipawat from the Noun Project https://thenounproject.com/term/drum/2306361
+Doumbek soundfont http://www.khafif.com/rhy/doumbek-soundfont.html
--- app.js
+++ app.js
@@ -1,41 +1,36 @@
-// Doumbek soundfont
-// http://www.khafif.com/rhy/doumbek-soundfont.html
+const DEFAULT_SOUNDFONT =  'Doumbek-Faisal';
+const DEFAULT_INSTRUMENT = 'doumbek';
+const DEFAULT_BUTTONS = {
+  "b1": 60,
+  "b2": 61,
+  "b3": 62,
+  "b4": 63,
+  "b5": 64,
+  "b6": 65
+};
 
-// Melodic Pool:
-// KEY	Midi	SOUND
-// 60	C5	Doum Back
-// 61	C#5	Doum Front
-// 62	D5	Finger Doum Back
-// 63	D#5	Finger Doum Front
-// 64	E5	Rim Doum Front
-// 65	F5	Tek Front
-// 66	F#5	Soft Tek Front
-// 67	G5	Half Tek Front
-// 68	G#5	Slap Front
-// 69	A5 	Half Slap Front
+// soundfonts.json
+let soundfonts = null;
 
-// Percussive Pool:
-// (This one matches whats at http://www.khafif.com/rhy/):
-// They are channel 10 (drum sound) keys.
-
-// KEY - SOUND
-// 64 - Doum Front
-// 62 - Tek Front
-// 45 - Half Slap Front
-// 35 - Slap Front
-// 63 - Half Tek
-// 70 - Doum Back
-// 71 - Finger Doum Back
-// 72 - Finger Doum Front
-// 73 - Rim Doum Front
-// 74 - Soft Tek Front
-
+// Current values
+let soundfont = DEFAULT_SOUNDFONT;
+let instrument = DEFAULT_INSTRUMENT;
+let buttons = DEFAULT_BUTTONS;
 let drums = null;
+
+async function loadSoundFonts() {
+  if (!soundfonts) {
+    soundfonts = await fetch('/soundfonts.json')
+      .then(data => data.json())
+      .then(json => json)
+  }
+}
+
 async function loadInstrument() {
   return new Promise((resolve, reject) => {
-    Soundfont.instrument(new AudioContext(), 'doumbek', { soundfont: 'Doumbek-Faisal', nameToUrl: function(name, soundfont, format) {
+    Soundfont.instrument(new AudioContext(), instrument, { soundfont, nameToUrl: function(name, sf, format) {
       format = format || 'mp3';
-      return `/drums/${soundfont}/${name}-${format}.js`;
+      return `/drums/${sf}/${name}-${format}.js`;
     }})
     .then(instrument => {
       resolve(instrument);
@@ -47,15 +42,7 @@ async function playButton(button) {
   if (!drums) {
     drums = await loadInstrument();
   }
-  const buttonToSound = {
-    'b1': 60,
-    'b2': 61,
-    'b3': 62,
-    'b4': 63,
-    'b5': 64,
-    'b6': 65,
-  }
-  drums.play(buttonToSound[button], 0, { gain: 10 });
+  drums.play(buttons[button], 0, { gain: 10 });
 }
 
 async function playDrum(event) {
@@ -66,7 +53,7 @@ async function playDrum(event) {
 }
 
 async function playKey(event) {
-  const sounds = {
+  const keys = {
     'Digit1': 'b1',
     'Digit2': 'b2',
     'Digit3': 'b3',
@@ -74,9 +61,9 @@ async function playKey(event) {
     'Digit5': 'b5',
     'Digit6': 'b6'
   }
-  if (event.code in sounds) {
+  if (event.code in keys) {
     event.preventDefault();
-    playButton(sounds[event.code]);
+    playButton(keys[event.code]);
   }
 }
 
@@ -85,13 +72,17 @@ function setViewportHeight() {
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
 
-setViewportHeight();
-window.addEventListener('resize', () => {
-  setTimeout(setViewportHeight, 100);
+window.addEventListener('DOMContentLoaded', async () => {
+  setViewportHeight();
+  window.addEventListener('resize', () => {
+    setTimeout(setViewportHeight, 100);
+  });
+
+  const drumkit = document.querySelector('.drumkit');
+  drumkit.addEventListener('click', playDrum);
+  drumkit.addEventListener('touchstart', playDrum);
+
+  document.addEventListener('keydown', playKey);
+
+  await loadSoundFonts();
 });
-
-const drumkit = document.querySelector('.drumkit');
-drumkit.addEventListener('click', playDrum);
-drumkit.addEventListener('touchstart', playDrum);
-
-document.addEventListener('keydown', playKey);
