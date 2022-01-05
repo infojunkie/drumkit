c63cb0b9cc5ebf2e2adbd96d6fb8ec5c1fc56be0
Karim Ratib <karim.ratib@gmail.com>
2022-01-05 20:39:17 +0200

    Service Worker implementation

---
+++ sw.js
@@ -0,0 +1,37 @@
+const version = "0.0.1";
+const cacheName = `drumkit-${version}`;
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/app.js',
+        '/soundfont-player.min.js',
+        '/hammer.min.js',
+        '/soundfonts.json'
+      ])
+      .then(() => self.skipWaiting());
+    })
+  );
+});
+
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request).then((response) => {
+        return caches.open(cacheName).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
