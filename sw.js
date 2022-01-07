4b9664a4bcdb6f5296b0ec88606297d014da6331
Karim Ratib <karim.ratib@gmail.com>
2022-01-07 13:34:00 +0200

    Code cleanup

--- sw.js
+++ sw.js
@@ -23,11 +23,11 @@ self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return resp || fetch(event.request).then((response) => {
-        return caches.open(cacheName).then((cache) => {
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request).then(response => {
+        return caches.open(cacheName).then(cache => {
           cache.put(event.request, response.clone());
           return response;
         });
